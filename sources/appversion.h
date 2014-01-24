61b185d27a3be145f7396e7f8592a5819adc2ffa
Xaviou <x.psoud@gmail.com>
2014-01-24 15:51:20 +0100

    Switched to version 1.0.0

--- sources/appversion.h
+++ sources/appversion.h
@@ -1,16 +1,17 @@
 #ifndef __APPVERSION_H_INCLUDED__
 #define __APPVERSION_H_INCLUDED__
 
-#define VERSION_MAJOR	0
-#define VERSION_MINOR	1
+#define VERSION_MAJOR	1
+#define VERSION_MINOR	0
 #define VERSION_REV     0
 // Add "201" before the number below to obtain
 // the complete date in format : YYYYMMJJ
-#define VERSION_BUILD   40122
+#define VERSION_BUILD   40124
 
 #define PRODUCTNAME "ScreenShotCleaner"
 #define COMPANYNAME "X.P. Softs"
 #define DESCRIPTION "Utility to cleanup screenshots images"
+#define wDESCRIPTION _("Utility to cleanup screenshots images")
 
 #define COPYRIGHT "Copyright (c) X.P. Softs 2014"
 
